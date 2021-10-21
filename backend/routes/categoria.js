ed755a9a344725a7da86392448f3557134bdd759
Alvaro Hernandez <alvarojhr96@gmail.com>
2021-10-20 19:06:10 -0500

    Divide backend

--- backend/routes/categoria.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const CategoriaController = require("../controllers/categoria");
-
-router.post("", CategoriaController.CreateCategoria);
-router.get("", CategoriaController.GetAll);
-
-module.exports = router;
