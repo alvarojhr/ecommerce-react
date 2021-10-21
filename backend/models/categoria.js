fc6efc8143cb860a6b3349212382fff0fa3c4d93
Alvaro Hernandez <alvarojhr96@gmail.com>
2021-10-20 19:08:06 -0500

    Deleting backend

--- backend/models/categoria.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const categoriaSchema = mongoose.Schema({
-  codigo: { type: String, required: true },
-  nombre: { type: String, required: true },
-});
-
-module.exports = mongoose.model("Categoria", categoriaSchema);
